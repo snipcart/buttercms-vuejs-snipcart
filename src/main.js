99c5e07beeaf94728d0ecb9fb6aab56f1a687855
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2018-05-02 11:50:18 -0400

    Register price formatting filter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import numeral from 'numeral';
+
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.filter('price', value => numeral(value).format('0.00'));
 
 /* eslint-disable no-new */
 new Vue({
