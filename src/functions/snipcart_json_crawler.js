00d3b72b9f992e4db053988630078cdc9e3be15d
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2018-05-01 20:52:37 -0400

    Set Content-Type for json crawler

--- src/functions/snipcart_json_crawler.js
+++ src/functions/snipcart_json_crawler.js
@@ -22,6 +22,9 @@ exports.handler = function(event, context, callback) {
 
       callback(null, {
       	statusCode: 200,
+      	headers: {
+      		"Content-Type": "application/json"
+      	},
       	body: JSON.stringify(products),
       });
     });
