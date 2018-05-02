3addfa11590cdf52fe0d83f1d2ee0d973c26e512
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2018-05-01 20:45:06 -0400

    Add json crawler function

---
+++ src/functions/snipcart_json_crawler.js
@@ -0,0 +1,28 @@
+
+var butter = require('buttercms')("e155d849518412ee7787af8cf61bb67840ee8c8e");
+
+exports.handler = function(event, context, callback) {
+
+  // snipcart send a GET request when crawling products
+  if(event.httpMethod !== 'GET') {
+    callback(null, {
+      statusCode: 200,
+      body: ''
+    });
+  }
+  
+  butter.page.list('product')
+    .then((res) => {
+      var products = res.data.data.map((product) => {
+      	return {
+      		id: product.fields.product_id,
+      		price: product.fields.price,
+      	};
+      });
+
+      callback(null, {
+      	statusCode: 200,
+      	body: JSON.stringify(products),
+      });
+    });
+}
